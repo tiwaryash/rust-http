b43b68d54707bda9dcef1cbf36bc6e791de71539
Yash-9274 <yash.tiwary20@st.niituniversity.in>
2025-03-30 12:18:41 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 #[allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener,TcpStream};
+use std::io::{Read,Write};
 
+//Handling an incoming client connection
+fn handle_client_req(mut stream: TcpStream){
+
+    let mut buffer=[0;1024];
+
+    if let Ok(_) = stream.read(&mut buffer){
+        let response ="HTTP/1.1 200 OK\r\n\r\n";
+        stream.write_all(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+}
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -11,8 +23,8 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_client_req(stream)
             }
             Err(e) => {
                 println!("error: {}", e);
